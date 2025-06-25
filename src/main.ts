5f5148df08614a46274e86ba8798e6e9ec959475
Timi-Leyin <timileyinoyelekan11@gmail.com>
2025-06-25 17:04:24 +0100

    chore: added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder } from '@nestjs/swagger';
 
 const PORT = 9999; // temp removed port to debug why it's at 4545 even though env is set to 9999
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // swagger does setup
+  const config = new DocumentBuilder()
+    .setTitle('Mi-Love API')
+    .setDescription('Mi-Love API')
+    .setVersion('1.0')
+    .addTag('mi-love')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   await app.listen(PORT, () => {
     console.log(`😉 Server is running on port http://localhost:${PORT}`);
   });
