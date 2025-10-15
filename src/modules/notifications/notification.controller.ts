550efc395deab214fb1604c537baa9fdd1c28fdf
Timi-Leyin <timileyinoyelekan11@gmail.com>
2025-10-15 18:53:28 +0100

    fix: add auth guard to notification

--- src/modules/notifications/notification.controller.ts
+++ src/modules/notifications/notification.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { PaginationParams } from '@/common/services/pagination.service';
 import { User } from '@/common/decorator/user.decorator';
 import { UserWithoutPassword } from '@/common/types/db';
+import { JwtAuthGuard } from '@/common/guards/jwt-auth.guard';
 
 @Controller('notifications')
+@UseGuards(JwtAuthGuard)
 export class NotificationController {
   constructor(private readonly notificationService: NotificationService) {}
 
